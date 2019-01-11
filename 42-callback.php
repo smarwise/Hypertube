b8ec1ffa41f0e1d236b64acabd33d5fc88ed3f48
smarwise <smarwise@e5r6p11.wethinkcode.co.za>
2019-01-11 12:23:46 +0200

    42 oauth

---
+++ 42-callback.php
@@ -0,0 +1,46 @@
+<html>
+<h1> GOT YOUR INFO</h1>
+</html>
+<?php
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+session_start();
+
+$authorization_code = $_GET['code'];
+
+$client_id = '61d50a325b359a90c18726e2bf5c95c8c914ce04f80cd5a0b26c7a0af166d397';
+$client_secret = '4559a71b8cc800004a5b90cd14c0faad1ff1bde08e6f5ff9716233f726362498';
+$redirect_uri = "http://localhost:8080/Hypertube/42-callback.php";
+$url = 'https://api.intra.42.fr/oauth/token';
+$data = array(
+    'code' => $authorization_code,
+    'client_id' => $client_id,
+    'client_secret' => $client_secret,
+    'redirect_uri' => $redirect_uri,
+    'grant_type' => "authorization_code"
+ );
+$options = array(
+    'http' => array(
+        'header'  => "Content-type: application/json\r\n",
+        'method'  => 'POST',
+        'content' => json_encode($data)
+    )
+);
+$context  = stream_context_create($options);
+$result = file_get_contents($url, false, $context);
+
+$array = json_decode($result, true);
+$access_token = $array['access_token'];
+$info = file_get_contents('https://api.intra.42.fr/v2/me?access_token=' . $access_token);
+$arr = json_decode($info, true);
+echo "<pre>";
+print_r($arr);
+$_SESSION["id"] = $arr["id"];
+$_SESSION["first_name"] = $arr["first_name"];
+$_SESSION["last_name"] = $arr["last_name"];
+$_SESSION["picture"] = $arr['image_url'];
+$_SESSION["email"] = $arr["email"];
+if ($_SESSION['id'])
+    header('Location: home.php');
+?>
--- login.php
+++ login.php
@@ -10,6 +10,7 @@ $loginurl = $helper->getLoginUrl($redirecturl, $permissions);
 // echo $loginurl;
 
 $gloginurl = $client->createAuthUrl();
+$loginurl42 = "https://api.intra.42.fr/oauth/authorize?client_id=61d50a325b359a90c18726e2bf5c95c8c914ce04f80cd5a0b26c7a0af166d397&redirect_uri=http%3A%2F%2Flocalhost%3A8080%2FHypertube%2F42-callback.php&response_type=code";
 ?>
 
 <html>
@@ -29,7 +30,8 @@ $gloginurl = $client->createAuthUrl();
     <input name="password" type="password" placeholder="Password"><br />
     <input type="submit" value="Login"><br />
     <input type="button" onclick="window.location='<?php echo $loginurl ?>';" value="Login with Facebook"><br />
-    <input type="button" onclick="window.location='<?php echo $gloginurl ?>';" value="Login with Google">
+    <input type="button" onclick="window.location='<?php echo $gloginurl ?>';" value="Login with Google"><br />
+    <input type="button" onclick="window.location='<?php echo $loginurl42 ?>';" value="Login with 42">
     </form>
     </body>
 </html>
